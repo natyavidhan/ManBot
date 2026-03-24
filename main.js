573ca08456b96fc9f05503df0fd2593e44100e2c
emty <workforemty@gmail.com>
2026-03-24 10:58:20 +0000

    lewd

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-yo
+const { Client, GatewayIntentBits } = require("discord.js")
+const client = new Client({
+     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+     ] 
+    });
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    message.reply({
+        content: "WE NEED TO ADD STUFF"
+    });
+    console.log(message)
+});
+client.login(
+    "TOKEN MAN"
+) 
